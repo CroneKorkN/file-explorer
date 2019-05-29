0f8cbb6b2aa188f65bb2da7bc9b682000b930900
mwie <mwiegand@seibert-media.net>
2019-05-29 21:59:11 +0200

    namespace scope test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get '/upload', to: 'index#new'
-  post '/', to: 'index#create'
-  get '/', to: 'index#index'
-  get '/:path', to: 'index#path', constraints: { path: /.+/ }
-  delete '/:path', to: 'index#delete', constraints: { path: /.+/ }
+  namespace ENV['URL_SCOPE'].to_sym do
+    get '/upload', to: 'index#new'
+    post '/', to: 'index#create'
+    get '/', to: 'index#index'
+    get '/:path', to: 'index#path', constraints: { path: /.+/ }
+    delete '/:path', to: 'index#delete', constraints: { path: /.+/ }
+  end
 end
