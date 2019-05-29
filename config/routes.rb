f310d9c64b37b225f4f98d5fbd1116fadf90c6ed
mwie <mwiegand@seibert-media.net>
2019-05-30 01:49:32 +0200

    optional url_scope routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  scope "/#{ENV['URL_SCOPE']}" do
+  routes = ->{
     get '/upload', to: 'index#new'
     post '/', to: 'index#create'
     get '/', to: 'index#index'
     get '/:path', to: 'index#path', constraints: { path: /.+/ }
     delete '/:path', to: 'index#delete', constraints: { path: /.+/ }
+  }
+  if ENV['URL_SCOPE']
+    scope "/#{ENV['URL_SCOPE']}" do
+      routes.call
+    end
+  else
+    routes.call
   end
 end
